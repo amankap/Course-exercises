ae3055e8e6e6e9767235fe4ca2cb4f520a360222
amankap <47714724+amankap@users.noreply.github.com>
2019-04-17 21:30:50 +0600

    Задание 1

---
+++ js/budget-request.js
@@ -0,0 +1,22 @@
+let money = prompt("Ваш бюджет на месяц?", ""),
+ time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let appData = {
+budget: money,
+timeData: time,
+expenses: {},
+optionalExpenses: [],
+income: [],
+savings: false
+};
+
+let question1 = prompt("Введите обязательную статью расходов в этом месяце", ""), 
+   question2 = prompt("Во сколько обойдется?", ""),
+   question3 = prompt("Введите обязательную статью расходов в этом месяце", "");
+   question4 = prompt("Во сколько обойдется?", ""),
+   
+
+   appData.expenses.question1 = question2;
+   appData.expenses.question3 = question4;
+
+   alert('Остаток на счете: ' + appData.budget / 30 + ' тенге');
